19093dba5afdbc69aa2a57a4c496f1b2e172c58b
Alex Couper <acouper@syndis.is>
2023-03-18 09:40:06 +0000

    Update description

--- cmd/root.go
+++ cmd/root.go
@@ -17,16 +17,14 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var (
 	// Used for flags
-	// TODO: Description update
 	rootCmd = &cobra.Command{
 		Use:   "iris-api",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "CLI for the Iris API",
+		Long: `CLI for using the IRIS API
+		
+Some useful env variables to ensure are set are:
+- IRIS_API_TOKEN
+		`,
 		// Uncomment the following line if your bare application
 		// has an action associated with it:
 		// Run: func(cmd *cobra.Command, args []string) {
