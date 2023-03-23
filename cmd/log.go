b82be528f0f1ed95381ef2b55b9c332e8e5ea3b8
Alex Couper <acouper@syndis.is>
2023-03-23 22:24:08 +0000

    Working piped/stdin version for logging

--- cmd/log.go
+++ cmd/log.go
@@ -5,8 +5,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -25,7 +29,6 @@ the token config in iris. Simply pass in any string and it will appear there.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := cmd.Context()
-		client := ctx.Value(clientKey).(*openapi.ClientWithResponses)
 
 		if len(args) > 0 {
 			// Log a single message
@@ -34,36 +37,98 @@ the token config in iris. Simply pass in any string and it will appear there.
 				Message:   args[0],
 				Timestamp: time.Now().UnixMilli(),
 			})
-			foo := openapi.SubmitLogsForTokenJSONRequestBody(logs)
-
-			resp, err := client.SubmitLogsForToken(ctx, foo)
-
-			if err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "Error: %s", err)
-				return
-			}
-
-			// TODO: Move this stuff into some generic error handler
-			// TODO: Add tests around status codes of the commands
-			switch code := resp.StatusCode; {
-			case code == http.StatusUnauthorized:
-				fmt.Fprintf(cmd.ErrOrStderr(), "Unauthorized")
-			case code == http.StatusForbidden:
-				fmt.Fprintf(cmd.ErrOrStderr(), "Forbidden")
-			case code >= 500:
-				fmt.Fprintf(cmd.ErrOrStderr(), "Server Error: %d", code)
-			case code <= 200:
-			default:
-				fmt.Fprintf(cmd.OutOrStdout(), "Unrecognized status code %d", code)
-			}
+			upload(ctx, logs, cmd.ErrOrStderr())
 
 		} else {
 			// Assume we're in stdin mode.
-			// TODO: Batch up and submit every so often, let's say once every 10 seconds for now
-			// and also on exit
+			// 4 channels
+			//  - message handler
+			//	- stop (used to indicate exit)
+			//  - ticker (controls how frequently we save)
+			//  - done (used to force main thread to wait for goroutine completion)
+
+			// 1 worker
+			//  - api_worker: Add scanned text to a list. If it gets told by ticker to upload
+			//                do that.
+
+			scanner := bufio.NewScanner(os.Stdin)
+
+			messages := make(chan string)
+			done := make(chan bool)
+			stop := make(chan bool)
+
+			var uploadFrequency time.Duration = 3 * time.Second
+			ticker := time.NewTicker(uploadFrequency)
+
+			go api_worker(messages, ticker.C, stop, done, ctx, cmd.ErrOrStderr())
+
+			for scanner.Scan() {
+				messages <- scanner.Text()
+			}
+
+			stop <- true
+
+			<-done
+
+			close(messages)
+
 		}
 
 	},
+}
+
+func upload(ctx context.Context, logs []openapi.SubmitLogEvent, stdErr io.Writer) {
+	if len(logs) == 0 {
+		return
+	}
+	client := ctx.Value(clientKey).(*openapi.ClientWithResponses)
+
+	foo := openapi.SubmitLogsForTokenJSONRequestBody(logs)
+
+	resp, err := client.SubmitLogsForToken(ctx, foo)
+
+	if err != nil {
+		fmt.Fprintf(stdErr, "Error: %s", err)
+		return
+	}
+
+	// TODO: Move this stuff into some generic error handler
+	// TODO: Add tests around status codes of the commands
+	switch code := resp.StatusCode; {
+	case code == http.StatusUnauthorized:
+		fmt.Fprintf(stdErr, "Unauthorized")
+	case code == http.StatusForbidden:
+		fmt.Fprintf(stdErr, "Forbidden")
+	case code >= 500:
+		fmt.Fprintf(stdErr, "Server Error: %d", code)
+	case code <= 200:
+	default:
+		fmt.Fprintf(stdErr, "Unrecognized status code %d", code)
+	}
+
+}
+func api_worker(messages <-chan string, control <-chan time.Time, stop <-chan bool, done chan<- bool, ctx context.Context, stdErr io.Writer) {
+	logs := make([]openapi.SubmitLogEvent, 0, 1)
+	count := 0
+	for {
+		select {
+		case message := <-messages:
+			logs = append(logs, openapi.SubmitLogEvent{
+				Message:   message,
+				Timestamp: time.Now().UnixMilli(),
+			})
+			count += 1
+		case <-stop:
+			upload(ctx, logs, stdErr)
+			done <- true
+			return
+		case <-control:
+			fmt.Printf("%s: Total received %d\n", time.Now().Format(time.RFC3339), count)
+			upload(ctx, logs, stdErr)
+			logs = nil
+		}
+	}
+
 }
 
 func init() {
