371eafe7b6685935d26f8cf527fb49c9a006c189
Alex Couper <acouper@syndis.is>
2023-05-03 13:21:17 +0000

    Add support for uploading a json file

--- cmd/submit.go
+++ cmd/submit.go
@@ -7,15 +7,20 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/spf13/cobra"
 	openapi "github.com/syndis-software/aftra-api/pkg/openapi"
 )
 
 var (
+	submitCmd_filename string
+	submitCmd_message  string
+
 	submitCmd = &cobra.Command{
 		Use:   "submit [scan-type] [scan-name] [scan-result]",
-		Args:  cobra.MatchAll(cobra.ExactArgs(3), cobra.OnlyValidArgs),
+		Args:  cobra.MatchAll(cobra.ExactArgs(2), cobra.OnlyValidArgs),
 		Short: "Submit a json formatted scan result",
 		Long: `Submit a json formatted scan result
 	
@@ -25,18 +30,31 @@ nessus format for syndis scans.
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
 			client := ctx.Value(clientKey).(*openapi.ClientWithResponses)
-			scanType, scanName, message := args[0], args[1], args[2]
+			scanType, scanName := args[0], args[1]
 
 			switch {
 			case ScanType(scanType) == syndis:
 
-				// Submit a set of scan events
-
 				var scans []openapi.SyndisInternalScanEvent
-				err := json.Unmarshal([]byte(message), &scans)
-
-				if err != nil {
-					return err
+				// Submit a set of scan events
+				if submitCmd_filename != "" {
+					jsonFile, err := os.Open(submitCmd_filename)
+					// if we os.Open returns an error then handle it
+					if err != nil {
+						return err
+					}
+					// defer the closing of our jsonFile so that we can parse it later on
+					defer jsonFile.Close()
+					byteValue, _ := ioutil.ReadAll(jsonFile)
+					err = json.Unmarshal(byteValue, &scans)
+					if err != nil {
+						return err
+					}
+				} else {
+					err := json.Unmarshal([]byte(submitCmd_message), &scans)
+					if err != nil {
+						return err
+					}
 				}
 
 				resp, err := client.SubmitScanResults(ctx, scanName, scans)
@@ -55,6 +73,14 @@ nessus format for syndis scans.
 )
 
 func init() {
+
+	submitCmd.Flags().StringVarP(&submitCmd_filename, "filename", "f", "", "JSON file to submit")
+	submitCmd.Flags().StringVarP(&submitCmd_message, "message", "m", "", "JSON string to submit")
+
+	// Want this, but no way to clear flag values between tests at the moment
+	// https://github.com/spf13/cobra/issues/1180
+	// submitCmd.MarkFlagsMutuallyExclusive("filename", "message")
+
 	rootCmd.AddCommand(submitCmd)
 
 }
--- cmd/submit_test.go
+++ cmd/submit_test.go
@@ -5,7 +5,9 @@ import (
 	"context"
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -52,7 +54,8 @@ func Test_ExecuteSubmit_Single_ServerResponseHandling(t *testing.T) {
 
 	header := make(http.Header, 1)
 	header.Set("Content-Type", "application/json")
-
+	submitCmd_filename = ""
+	submitCmd_message = ""
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			mockDoer := &MockHTTP{
@@ -67,7 +70,7 @@ func Test_ExecuteSubmit_Single_ServerResponseHandling(t *testing.T) {
 			actual := new(bytes.Buffer)
 			rootCmd.SetOut(actual)
 			rootCmd.SetErr(actual)
-			rootCmd.SetArgs([]string{"submit", "syndis", "scan-name", `[{"folder_name":"Islenskur_Texti","scan_id":"117","scan_uuid":"d776c35d-7d0e-b699-d035-a0120d2dfe28a871ce19964f5207","scan_start":"2023-04-04 08:25:22","scan_end":"2023-04-04 08:32:35","file_name":"External.csv","plugin_id":10114,"cve":"CVE-1999-0524","cvss":0,"risk":"None","host":"153.92.147.3","protocol":"icmp","port":"0","name":"ICMP Timestamp Request Remote Date Disclosure","synopsis":"It is possible to determine the exact time set on the remote host.","description":"The remote host answers to an ICMP timestamp request.  This allows an\nattacker to know the date that is set on the targeted machine, which\nmay assist an unauthenticated, remote attacker in defeating time-based\nauthentication protocols.\n\nTimestamps returned from machines running Windows Vista / 7 / 2008 /\n2008 R2 are deliberately incorrect, but usually within 1000 seconds of\nthe actual system time.","solution":"Filter out the ICMP timestamp requests (13), and the outgoing ICMP\\ntimestamp replies (14).","see_also":"","plugin_output":"The remote clock is synchronized with the local clock.\\n","stig_severity":"","cvss_v3_0_base_score":0,"cvss_temporal_score":0,"cvss_v3_0_temporal_score":0,"risk_factor":"None","bid":"","xref":"CWE:200","mskb":"","plugin_publication_date":"1999/08/01","plugin_modification_date":"2019/10/04","metasploit":"","core_impact":"","canvas":""}]`})
+			rootCmd.SetArgs([]string{"submit", "syndis", "scan-name", "--message", `[{"folder_name":"Islenskur_Texti","scan_id":"117","scan_uuid":"d776c35d-7d0e-b699-d035-a0120d2dfe28a871ce19964f5207","scan_start":"2023-04-04 08:25:22","scan_end":"2023-04-04 08:32:35","file_name":"External.csv","plugin_id":10114,"cve":"CVE-1999-0524","cvss":0,"risk":"None","host":"153.92.147.3","protocol":"icmp","port":"0","name":"ICMP Timestamp Request Remote Date Disclosure","synopsis":"It is possible to determine the exact time set on the remote host.","description":"The remote host answers to an ICMP timestamp request.  This allows an\nattacker to know the date that is set on the targeted machine, which\nmay assist an unauthenticated, remote attacker in defeating time-based\nauthentication protocols.\n\nTimestamps returned from machines running Windows Vista / 7 / 2008 /\n2008 R2 are deliberately incorrect, but usually within 1000 seconds of\nthe actual system time.","solution":"Filter out the ICMP timestamp requests (13), and the outgoing ICMP\\ntimestamp replies (14).","see_also":"","plugin_output":"The remote clock is synchronized with the local clock.\\n","stig_severity":"","cvss_v3_0_base_score":0,"cvss_temporal_score":0,"cvss_v3_0_temporal_score":0,"risk_factor":"None","bid":"","xref":"CWE:200","mskb":"","plugin_publication_date":"1999/08/01","plugin_modification_date":"2019/10/04","metasploit":"","core_impact":"","canvas":""}]`})
 
 			ctx := context.WithValue(context.Background(), doerKey, mockDoer)
 			submitCmd.SetContext(ctx)
@@ -115,7 +118,6 @@ func Test_ExecuteSubmit_Single_JsonParsing(t *testing.T) {
 
 	header := make(http.Header, 1)
 	header.Set("Content-Type", "application/json")
-
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			mockDoer := &MockHTTP{
@@ -130,12 +132,83 @@ func Test_ExecuteSubmit_Single_JsonParsing(t *testing.T) {
 			actual := new(bytes.Buffer)
 			rootCmd.SetOut(actual)
 			rootCmd.SetErr(actual)
-			rootCmd.SetArgs([]string{"submit", "syndis", "scan-name", tc.message})
+			rootCmd.SetArgs([]string{"submit", "syndis", "scan-name", "--message", tc.message})
 
 			ctx := context.WithValue(context.Background(), doerKey, mockDoer)
 			submitCmd.SetContext(ctx)
 
 			err := rootCmd.ExecuteContext(ctx)
+
+			var expectedRequests int
+
+			if tc.errorExpected {
+				assert.NotNil(t, err)
+				expectedRequests = 0
+			} else {
+				assert.Equal(t, nil, err)
+				expectedRequests = 1
+			}
+
+			assert.Equal(t, expectedRequests, len(mockDoer.Requests))
+			assert.Equal(t, tc.expectedOutput, actual.String())
+		})
+	}
+
+}
+
+func Test_ExecuteSubmit_File(t *testing.T) {
+	type test struct {
+		fileContent    string
+		expectedOutput string
+		errorExpected  bool
+	}
+
+	tests := map[string]test{
+		"success": {
+			fileContent:    "[{}]",
+			expectedOutput: "",
+			errorExpected:  false,
+		},
+		"malformed-json": {
+			fileContent:    "ooo",
+			expectedOutput: "Error: invalid character 'o' looking for beginning of value\n",
+			errorExpected:  true,
+		},
+	}
+
+	submitCmd_filename = ""
+	submitCmd_message = ""
+	header := make(http.Header, 1)
+	header.Set("Content-Type", "application/json")
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+
+			file, err := ioutil.TempFile("/tmp", "submit-test")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer os.Remove(file.Name())
+			file.Write([]byte(tc.fileContent))
+
+			mockDoer := &MockHTTP{
+				Response: http.Response{
+					StatusCode: 200,
+					Status:     "",
+					Body:       ioutil.NopCloser(bytes.NewBufferString("")),
+					Header:     header,
+				},
+				ResponseError: nil,
+			}
+			actual := new(bytes.Buffer)
+			rootCmd.SetOut(actual)
+			rootCmd.SetErr(actual)
+			rootCmd.SetArgs([]string{"submit", "syndis", "scan-name", "--filename", file.Name()})
+
+			ctx := context.WithValue(context.Background(), doerKey, mockDoer)
+			submitCmd.SetContext(ctx)
+
+			err = rootCmd.ExecuteContext(ctx)
 
 			var expectedRequests int
 
